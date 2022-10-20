cc1eea831fbc0f2fbbfd601561c8d2ec232f69ab
Michael Hanselmann <public@hansmi.ch>
2022-10-20 22:30:44 +0200

    Switch to kingpin for flag parsing
    Newer versions of github.com/prometheus/exporter-toolkit require a more
    complex type as the configuration given to `web.ListenAndServe`. The
    upstream package only provides a kingpin implementation of the flag
    code.

--- luxws-exporter/main.go
+++ luxws-exporter/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,27 +11,28 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/version"
 	"github.com/prometheus/exporter-toolkit/web"
+	"gopkg.in/alecthomas/kingpin.v2"
 
 	kitlog "github.com/go-kit/kit/log"
 )
 
-var listenAddress = flag.String("web.listen-address", ":8081", "The address to listen on for HTTP requests")
-var metricsPath = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics")
-var disableExporterMetrics = flag.Bool("web.disable-exporter-metrics", false, "Exclude metrics about the exporter itself")
-var maxConcurrent = flag.Uint("web.max-requests", 3, "Maximum number of concurrent scrape requests")
-var configFile = flag.String("web.config", "", "Path to config yaml file that can enable TLS or authentication")
+var listenAddress = kingpin.Flag("web.listen-address", "The address to listen on for HTTP requests").Default(":8081").String()
+var metricsPath = kingpin.Flag("web.telemetry-path", "Path under which to expose metrics").Default("/metrics").String()
+var disableExporterMetrics = kingpin.Flag("web.disable-exporter-metrics", "Exclude metrics about the exporter itself").Bool()
+var maxConcurrent = kingpin.Flag("web.max-requests", "Maximum number of concurrent scrape requests").Default("3").Uint()
+var configFile = kingpin.Flag("web.config", "Path to config yaml file that can enable TLS or authentication").String()
 
-var verbose = flag.Bool("verbose", false, "Log sent and received messages")
-var timeout = flag.Duration("scrape-timeout", time.Minute, "Maximum duration for a scrape")
+var verbose = kingpin.Flag("verbose", "Log sent and received messages").Bool()
+var timeout = kingpin.Flag("scrape-timeout", "Maximum duration for a scrape").Default("1m").Duration()
 
-var target = flag.String("controller.address", "",
-	`host:port for controller Websocket service (e.g. "192.0.2.1:8214")`)
-var httpTarget = flag.String("controller.address.http", "",
-	`host:port for controller HTTP service; used to retrieve time (e.g. "192.0.2.1:80")`)
-var timezone = flag.String("controller.timezone", time.Local.String(),
-	"Timezone for parsing timestamps")
-var lang = flag.String("controller.language", "",
-	fmt.Sprintf("Controller interface language (one of %q)", supportedLanguages()))
+var target = kingpin.Flag("controller.address",
+	`host:port for controller Websocket service (e.g. "192.0.2.1:8214")`).PlaceHolder("HOST:PORT").Required().String()
+var httpTarget = kingpin.Flag("controller.address.http",
+	`host:port for controller HTTP service; used to retrieve time (e.g. "192.0.2.1:80")`).PlaceHolder("HOST:PORT").String()
+var timezone = kingpin.Flag("controller.timezone",
+	"Timezone for parsing timestamps").Default(time.Local.String()).String()
+var lang = kingpin.Flag("controller.language",
+	fmt.Sprintf("Controller interface language (one of %q)", supportedLanguages())).PlaceHolder("NAME").Required().String()
 
 func supportedLanguages() []string {
 	result := []string{}
@@ -45,11 +45,7 @@ func supportedLanguages() []string {
 }
 
 func main() {
-	flag.Parse()
-
-	if *target == "" {
-		log.Fatal("Target address not specified")
-	}
+	kingpin.Parse()
 
 	opts := collectorOpts{
 		verbose:       *verbose,
